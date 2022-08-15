ec1f9d24b636d9d19e4e6b89c63c582c64b3de95
tuxuuyzhas <tuxuuyzhas@yandex.ru>
2022-08-15 22:08:58 +0300

    some configs and command handling

---
+++ src/game/command.py
@@ -0,0 +1,15 @@
+from .game import Actor
+
+class CommandHandler(): 
+    """
+    Команды игрового движка
+    """
+    def __init__(self):
+        """
+        init
+        """
+        pass
+    
+    def handle(self, command):
+        command_list = ['w', '']
+        
