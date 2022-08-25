737731d31e2f331c58770cbc76239149c86dbd30
idk <some@mail>
2022-08-25 21:50:41 +0300

    gamedata controller

---
+++ src/game/gamedata.py
@@ -0,0 +1,11 @@
+class Gamedata:
+    """
+    Управление игровыми данными, загружаемыми из файлов
+    """
+
+    def __init__(self, path: str):
+        self.data = None
+        self.path = path
+
+    def load_data(self, gamedata):
+        pass 
